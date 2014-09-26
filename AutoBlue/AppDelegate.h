421f1032ebf2a6a3679701cdc30326a83d582570
Michael Vosseller <michael.vosseller@gmail.com>
2014-09-26 01:32:19 -0400

    Template Project

---
+++ AutoBlue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoBlue
+//
+//  Created by Michael Vosseller on 9/26/14.
+//  Copyright (c) 2014 MPV Software, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AutoBlue/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  AutoBlue
+//
+//  Created by Michael Vosseller on 9/26/14.
+//  Copyright (c) 2014 MPV Software, LLC. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
